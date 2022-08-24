8dbd860b5b5abfe7cf45a43e289055e2dfc7b3a5
Ruel Mindo <wrelmind@gmail.com>
2022-08-24 21:49:34 +0800

    Added predefined event start

--- packages/native/src/dispatcher.js
+++ packages/native/src/dispatcher.js
@@ -1,53 +1,59 @@
 /**
  * Properties
  */
-import properties from './properties'
+ import properties from './properties'
 
-/**
- * merge dispatched states
- * @returns 
- */
-const merge = ({route, properties}, props) => {
-  var data = {}
-
-  for(var {scope, payload} of properties) {
-    if(scope) {
-
-      if(typeof scope == 'string') {
-        throw new Error(
-          'Expected scope to be of type `array` but received a `string`.'
-        )
-      }
-
-      if(scope.includes(route.name)) {
-        data = Object.assign(data, payload)
-      }
-    }
-    else {
-      data = Object.assign(data, payload)
-    }
-  }
-  return Object.assign(data, props)
-}
-
-
-/**
- * Set properties
- */
-export default (dispatch, context) => {
-  var props = properties(context)
-  /**
-   * Dispatch at once before the initial screen
-   */
-  if(props.navigate.history.length == 1) {
-    if(dispatch) {
-      if(typeof dispatch == 'function') {
-        context.properties.push({payload: dispatch(props)})
-      }
-      else {
-        context.properties.push({payload: dispatch})
-      }
-    }
-  }
-  return context.props = merge(context, props)
-}
+ /**
+  * Get dispatched states
+  * @returns 
+  */
+ const mergeProps = ({route, properties}, props) => {
+   var data = {}
+ 
+   for(var {scope, payload} of properties) {
+     if(scope) {
+ 
+       if(typeof scope == 'string') {
+         throw new Error(
+           'Expected scope to be of type `array` but received a `string`.'
+         )
+       }
+ 
+       if(scope.includes(route.name)) {
+         data = Object.assign(data, payload)
+       }
+     }
+     else {
+       data = Object.assign(data, payload)
+     }
+   }
+   return Object.assign(data, props)
+ }
+ 
+ 
+ /**
+  * 
+  */
+ export default (dispatch, context) => {
+   var props = properties(context)
+ 
+   /**
+    * Dispatch props
+    */
+   if(dispatch) {
+     if(typeof dispatch == 'function') {
+       context.properties.push({payload: dispatch(props)})
+     }
+     else {
+       context.properties.push({payload: dispatch})
+     }
+   }
+   /**
+    * Execute event before the initial screen mounted
+    */
+   if(props.navigate.history.length == 1) {
+     props.events.emit('initial')
+   }
+ 
+   return context.props = mergeProps(context, props)
+ }
